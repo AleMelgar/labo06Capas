f3bc5e099dd2365c17f2731d507d94877845f26a
AleMelgar <105059006+AleMelgar@users.noreply.github.com>
2025-06-26 21:19:43 -0600

    Delete desafio_simu directory

--- desafio_simu/src/pentagon.cpp
+++
@@ -1,5 +0,0 @@
-#include "pentagon.h"
-
-void DrawPentagon(Vector2 center, float radius, Color color) {
-    DrawPoly(center, 5, radius, -90, color);
-}
--- desafio_simu/src/pentagon.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <raylib.h>
-
-void DrawPentagon(Vector2 center, float radius, Color color);
